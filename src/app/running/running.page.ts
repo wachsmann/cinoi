d54e90d19051184734a6cb76525d5b08284ebc09
Hugo Tomazi Busnardo <hugo.busnardo@gmail.com>
2020-06-20 15:28:57 -0300

    Realizada correção de exibição na UI

--- src/app/running/running.page.ts
+++ src/app/running/running.page.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  GoogleMaps,
-  GoogleMap,
-} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-running',
@@ -11,31 +7,30 @@ import {
 })
 export class RunningPage implements OnInit {
 
-  private distance: string = "5,80";
-  private time: string = "00:30:12";
-  private calories: number = 670;
-  private average_speed: string = "5,80";
-  private target_speed: number = 10;
-  map: GoogleMap;
+  public distance = '5,80';
+  public time = '00:30:12';
+  public calories = 670;
+  public average_speed = '5,80';
+  public target_speed = 10;
   loading: any;
 
   constructor() { }
 
-  async ngOnInit() {
-    await this.loadMap()
+  ngOnInit() {
+    // await this.loadMap();
   }
 
   loadMap() {
-    this.map = GoogleMaps.create('map_canvas', {
-      camera: {
-        target: {
-          lat: 43.0741704,
-          lng: -89.3809802
-        },
-        zoom: 18,
-        tilt: 30
-      }
-    });
+    // this.map = GoogleMaps.create('map_canvas', {
+    //   camera: {
+    //     target: {
+    //       lat: 43.0741704,
+    //       lng: -89.3809802
+    //     },
+    //     zoom: 18,
+    //     tilt: 30
+    //   }
+    // });
 
   }
 
