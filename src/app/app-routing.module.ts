bb8cb3251764754d360290278a0e9df3c95acd31
Hugo Tomazi Busnardo <hugo.busnardo@gmail.com>
2020-06-21 23:14:34 -0300

    Corrigido Bugs no Merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule), canActivate: [AuthGuard] },
-  { path: 'trainings', loadChildren: () => import('./trainings/trainings.module').then( m => m.TrainingsPageModule) },
-  { path: 'running', loadChildren: () => import('./running/running.module').then( m => m.RunningPageModule) }
+  { path: 'trainings', loadChildren: () => import('./trainings/trainings.module').then( m => m.TrainingsPageModule), canActivate: [AuthGuard] },
+  { path: 'running', loadChildren: () => import('./running/running.module').then( m => m.RunningPageModule), canActivate: [AuthGuard] }
 ];
 
 @NgModule({
